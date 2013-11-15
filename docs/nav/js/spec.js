ea50202832bab5745a677d200a31eb5770d76f2d
Larry Botha <larry@fixate.it>
2013-11-15 14:09:12 +0200

    add untracked files

---
+++ docs/nav/js/spec.js
@@ -0,0 +1,19 @@
+(function(window, $) {
+
+  function toggle(el) {
+    var $el = $(el),
+        activeClass = 'menu-toggle--active';
+
+    if ($el.hasClass(activeClass)) {
+      $el.removeClass(activeClass);
+    } else {
+      $el.addClass(activeClass);
+    }
+  }
+
+  $('.menu-toggle').on('click touch', function(e) {
+    e.preventDefault();
+    toggle(this);
+  });
+
+}(this, this.jQuery));
